5e0f39f120e52d8b27857220f411a0472e57242e
Ali Shirsalimian <ashirsalimian12@gmail.com>
2023-04-12 08:03:42 -0500

    Reading from disk to memory. Posting Message.txt to console. In Blue.

--- boot.asm
+++ boot.asm
@@ -1,10 +1,43 @@
-ORG 0x7c00
+; bootloader that prints out "Hello World!". enabled video mode. color is blue.
+
+ORG 0
 BITS 16
 
+
+jmp 0x7c0:start
+
+
+
 start:
-    mov si, message
-    call print 
+    cli ; Clear Interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; Enables Interrupts
+
+    mov ah, 2 ; Read sector command
+    mov al, 1 ; One sector to read
+    mov ch, 0 ; Cylinder low eigth bits
+    mov cl, 2 ; Read sector two
+    mov dh, 0 ; Head number
+    mov bx, buffer
+    int 0x13
+    
+    jc error
+    
+    mov si, buffer
+    call print
+
     jmp $
+
+error:
+    mov si, error_message
+    call print
+    jmp $
+
 
 print:
     mov bx, 0
@@ -29,6 +62,12 @@ print_char:
     int 10h
     ret
 
-message: db 'Hello World', 0
+error_message: db 'Failed to load Sector' , 0
+
+
+
 times 510 - ($ - $$) db 0 
 dw 0xAA55
+
+
+buffer:
