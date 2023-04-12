02856da66e8a1993c5d85ebda42b84d1f017a80c
Ali Shirsalimian <ashirsalimian12@gmail.com>
2023-04-12 09:36:02 -0500

    Setup boot.asm to be in protected mode

--- boot.asm
+++ boot.asm
@@ -1,73 +1,71 @@
-; bootloader that prints out "Hello World!". enabled video mode. color is blue.
-
-ORG 0
+ORG 0x7c00
 BITS 16
 
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
-jmp 0x7c0:start
+_start:
+    jmp short start
+    nop
 
-
+times 33 db 0
 
 start:
+    jmp 0:step2
+
+step2:
     cli ; Clear Interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enables Interrupts
 
-    mov ah, 2 ; Read sector command
-    mov al, 1 ; One sector to read
-    mov ch, 0 ; Cylinder low eigth bits
-    mov cl, 2 ; Read sector two
-    mov dh, 0 ; Head number
-    mov bx, buffer
-    int 0x13
-    
-    jc error
-    
-    mov si, buffer
-    call print
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
+;GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+;offset 0x8
+gdt_code:       ;CS should point to this
+    dw 0xffff   ;Segment limit first 0-15 bits
+    dw 0        ;Base first 0-15 bits  
+    db 0        ;Base 16-23 bits
+    db 0x9a     ;Access byte
+    db 11001111b ;High 4 bit flags and the low 4 bit flags
+    db 0
+gdt_data:       ;DS, SS, ES, FS, GS
+    dw 0xffff   ;Segment limit first 0-15 bits
+    dw 0        ;Base first 0-15 bits  
+    db 0        ;Base 16-23 bits
+    db 0x92     ;Access byte
+    db 11001111b ;High 4 bit flags and the low 4 bit flags
+    db 0
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-
-print:
-    mov bx, 0
-    mov dl, 0
-    mov dh, 10
-.loop:
-    mov ah, 02h
-    int 10h
-    inc dl
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char 
-    jmp .loop
-.done:   
-    ret
-
-print_char:
-    mov ah, 09h
-    mov bl, 3
-    mov cx, 1
-    int 10h
-    ret
-
-error_message: db 'Failed to load Sector' , 0
-
-
 
 times 510 - ($ - $$) db 0 
 dw 0xAA55
-
-
-buffer:
